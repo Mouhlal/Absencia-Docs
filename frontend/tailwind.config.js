19d76a9c33955e45d646db9f854dc7a7d4044033
Mouhlal <akramemouhlal@gmail.com>
2025-08-01 12:04:54 +0100

    fix erreur tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './src/**/*.{js,ts,jsx,tsx}', // adapte selon ta structure
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
